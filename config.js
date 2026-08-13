e701d2acd7dca6a5c5b3ddf51e2889b6afe6e68e
seiji0528-hub <k.seiji0528@gmail.com>
2026-08-14 04:13:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const SUPABASE_URL = "https://doeevvuikcxovqihkefw.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://doeevvuikcxovqihkefw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_G4qfcMYF67VfFqInuhUDpw_UlYAYTJp";
 const ACCESS_CODE = "0";
